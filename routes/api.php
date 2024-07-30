45f3eae3a6dc68e7ca6dde5b4ab107c75823130e
morales.william <morales.william@correounivalle.edu.co>
2024-07-30 18:38:52 +0000

    create jwt auth

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,21 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\AuthController;
 Route::resource("/users", userController::class);
 Route::resource("/books", BookController::class);
 Route::resource("/authors", AuthorController::class);
 Route::resource("/book-categories", BookCategoryController::class);
 Route::resource("/categories", CategoryController::class);
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
+});
